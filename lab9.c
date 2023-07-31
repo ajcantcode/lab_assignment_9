cec0cbf63570787be4bc3b359a47ba82bc147065
ajcantcode <136856497+ajcantcode@users.noreply.github.com>
2023-07-30 23:24:08 -0400

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+	int	id;
+	char name;
+	int	order; 
 };
 
+#define hashSize 64 //arbitrary value?
 // Fill out this structure
 struct HashType
 {
-
+    struct RecordType * records[hashSize];
 };
 
 // Compute the hash function
 int hash(int x)
 {
-
-}
+    return x % hashSize;
+};
 
 // parses input file to an integer array
 int parseData(char* inputFileName, struct RecordType** ppData)
@@ -79,7 +81,11 @@ void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 
 	for (i=0;i<hashSz;++i)
 	{
-		// if index is occupied with any records, print all
+		if (pHashArray->records[i] != NULL)
+        {
+            printf("index %d -> %d %c %d", i, pHashArray->records[i]->id, pHashArray->records[i]->name, pHashArray->records[i]->order);
+            printf(" ");
+        }
 	}
 }
 
@@ -88,7 +94,25 @@ int main(void)
 	struct RecordType *pRecords;
 	int recordSz = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
+	recordSz = parseData("input.txt", &pRecords); //size of records?
 	printRecords(pRecords, recordSz);
 	// Your hash implementation
+	struct HashType hashStruct;
+	for(int n = 0; n < hashSize; n++)
+	{
+	    hashStruct.records[n] = NULL;
+	}
+	
+	for(int n = 0; n < recordSz; n++)
+	{
+	    struct RecordType *pRecord = pRecords + n;
+	    int index = hash(pRecord->id);
+	    hashStruct.records[index] = pRecord;
+	}
+	
+	displayRecordsInHash(&hashStruct, hashSize);
+	
+	free(pRecords);
+	
+	return 0;
 }
